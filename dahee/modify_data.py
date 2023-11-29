3b73a743687f219be6a1e1ba2ea285a2d3cfbb61
heedahan16 <devhee.handa@gmail.com>
2023-11-29 15:33:50 +0900

    update : modify_data.py : preprocess data for modeling

--- dahee/modify_data.py
+++ dahee/modify_data.py
@@ -3,40 +3,27 @@ pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_seq_items', None)
 
+# csv 파일에서 DataFrame을 읽어오기
 df = pd.read_csv('dahee/data/unique_data_code.csv', encoding='cp949')
-# print('df: ', df.shape)
-# print(df)
 
-# 연령별 dataframe 생성
-df_student = df[df['연령']=='청소년']
-df_adult = df[df['연령']=='성인']
-df_elderly= df[df['연령']=='노인']
+# 종목명 띄어쓰기 삭제
+for i in range(len(df['종목명'])):
+    df['종목명'][i] = df['종목명'][i].replace(' ', '')
+
+# 연령 변경
+df.loc[df['연령']=='청소년', '연령'] = '학생'
+print(df.head())
+
+# 시설주소 열 추가
+df['시설주소'] = None
+for i in range(len(df['지번주소'])):
+    df['시설주소'][i] = df['지번주소'][i].split()[1]
+
+# 지번주소 열 삭제
+df = df.drop(columns='지번주소')
+print(df.head())
 
 # 인코딩 후 csv 파일로 저장
-df_student.to_csv('dahee/data/df_student.csv', mode='w', encoding='cp949', index=False)
-df_adult.to_csv('dahee/data/df_adult.csv', mode='w', encoding='cp949', index=False)
-df_elderly.to_csv('dahee/data/df_elderly.csv', mode='w', encoding='cp949', index=False)
-
-
-# 중복되지 않는 데이터 종류 개수 확인
-# print(df.nunique())
-
-# # 중복값 제거
-# df_unique = df.drop_duplicates()
-# df_unique = df_unique.reset_index(drop=True)
-# print('unique: ', df_unique.shape)
-
-# print(df)
-# print(df.head())
-
-# 열 삭제
-# df = df.iloc[:, :10]
-# print(df.head())
-
-
-# # 시설명이 '강낭구민체육관'인 지번주소 변경
-# df.loc[df['시설명'] == '강남구민체육관', '지번주소'] = '서울 강남구 개포동 271'
-
-# print(df.head())
+df.to_csv('dahee/data/program.csv', mode='w', encoding='cp949', index=False)
 
 
